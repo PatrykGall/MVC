c349439167c0451da250971072b220324ead9de4
Patryk Galary <galarpa3745_aehit@students.vizja.pl>
2026-03-27 02:36:57 +0100

    Zapis ostateczny

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Adres: /polls/
     path('', views.index, name='index'),
+    # Adres: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
 ]
