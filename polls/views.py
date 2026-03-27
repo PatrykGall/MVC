70a33fbe34e6c726b038b8242701e7f1e4c922ee
Patryk Galary <galarpa3745_aehit@students.vizja.pl>
2026-03-27 02:27:32 +0100

    Finalna poprawka views i detail

--- polls/views.py
+++ polls/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Ankieta działa! Null bytes i błędy importu pokonane.")
+
+def detail(request, question_id):
+    return HttpResponse(f"Spoglądasz na pytanie numer {question_id}.")
