95e2d9839555b1bd0a1aa09c8672f390c53d6fe5
Patryk Galary <galarpa3745_aehit@students.vizja.pl>
2026-03-28 16:09:58 +0100

    Dodano HelloWorldController

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    // 
+    // GET: /HelloWorld/
+    public string Index()
+    {
+        return "To jest moja domyślna akcja (Index)...";
+    }
+
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    public string Welcome()
+    {
+        return "To jest metoda Welcome!";
+    }
+}
